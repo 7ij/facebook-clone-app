4c05e49e9e9e667a5a3e2a1b0a81edf5912a305f
Md. Ashiqur Rahman Naeem <70688154+7ij@users.noreply.github.com>
2024-11-05 02:50:13 +0600

    Revert "Added bottom nav view and also two fragment for testing purpose if bn…"
    This reverts commit 8d92426c0f8b55dced09b91449633fb780b92897.

--- app/src/main/java/com/nayeem2021/liilab_app_dev_assesment_project/MainActivity.kt
+++ app/src/main/java/com/nayeem2021/liilab_app_dev_assesment_project/MainActivity.kt
@@ -1,49 +1,14 @@
 package com.nayeem2021.liilab_app_dev_assesment_project
 
-import android.content.Context
 import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Log
-import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.nayeem2021.liilab_app_dev_assesment_project.databinding.ActivityMainBinding
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        initViews()
-        setContentView(binding.root)
-    }
 
-
-    private fun initViews() {
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home_page -> {
-                    replaceFragment(FeedFragment())
-                }
-
-                R.id.my_community_page -> {
-                    replaceFragment(MyCommunityFragment())
-                }
-
-                else -> {
-                    true
-                }
-            }
-
-        }
-
-        binding.bottomNavigationView.selectedItemId = R.id.home_page
-    }
-
-    private fun replaceFragment(fragment: Fragment) : Boolean {
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
-            .commit()
-        return true
     }
 }
